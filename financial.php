a7af25c55b3ca083769250735d341a605b53e667
gphilipose <ginaphilipose@gmail.com>
2025-02-02 10:56:41 -0800

    Update financial.php

--- financial.php
+++ financial.php
@@ -118,9 +118,9 @@ Reviews
 
 <?php
 $servername = "cssql.seattleu.edu";
-$username = "ll_gphilipose";
-$password = "r4BB4/4wX8eBo9bZ";
-$dbname = "ll_gphilipose";
+$username = "";
+$password = "";
+$dbname = "";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
