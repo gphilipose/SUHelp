629dc0a2dd73119e8f990c9724be354beeeead00
gphilipose <ginaphilipose@gmail.com>
2025-02-02 10:58:14 -0800

    Update health.php

--- health.php
+++ health.php
@@ -151,9 +151,9 @@
 
 <?php
 $servername = "cssql.seattleu.edu";
-$username = "ll_gphilipose";
-$password = "r4BB4/4wX8eBo9bZ";
-$dbname = "ll_gphilipose";
+$username = "";
+$password = "";
+$dbname = "";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
